eb5f8c7f441b9a7c6a66887feb0ccddb3cbc556f
w131376 <arayik.aslanyan.external@worldline.com>
2024-02-01 20:08:17 +0400

    main logic complete search.py

--- search.py
+++ search.py
@@ -1,6 +1,9 @@
 numbers = [1, 2, 3, 4, 5]
 
 search = 4
+is_exist = False
 for n in numbers:
-      if search == n:
-        print(True)
+    if search == n:
+        is_exist = True
+
+print(is_exist)
